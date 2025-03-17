9e7a7b48f11fe32369e8f2281b75d4f289185473
miningyu <miningyu1997@gmail.com>
2025-03-17 23:49:48 +0900

    fix: update blog branding and configuration details

--- slate.config.ts
+++ slate.config.ts
@@ -7,17 +7,17 @@ export default defineConfig({
   lang: 'en-US',
   site: 'https://slate-blog-demo.vercel.app',
   avatar: '/avatar.png',
-  title: 'Slate Blog',
-  description: 'Pure thoughts, simple stories.',
+  title: 'Mnngy Blog',
+  description: '안녕하세요. Mnngy의 블로그입니다. 웹 개발에 관심이 있습니다. 😌',
   lastModified: true,
   readTime: true,
   footer: {
-    copyright: '© 2025 Slate Design',
+    copyright: `© Mnngy's blog 2025 / Based on Slate Design template`,
   },
   socialLinks: [
     {
       icon: 'github',
-      link: 'https://github.com/SlateDesign/slate-blog'
+      link: 'https://github.com/miningyu'
     },
 ]
 });
